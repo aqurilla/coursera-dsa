0278cbfde237eb61a08b002320624e331dd3b1ce
aqurilla <aqurilla@gmail.com>
2019-07-05 00:33:06 -0400

    update quicksort

--- algorithmic_toolbox/python/quicksort.py
+++ algorithmic_toolbox/python/quicksort.py
@@ -1,21 +1,36 @@
-from numpy import random
+import numpy as np
+import random
 
 def Partition(A, l, r):
     x = A[l]
-    
-    while A[j]
-
-
+    j = l
+    for i in range(l+1, r+1):
+        if A[i]<=x:
+            j = j+1
+            A[j], A[i] = A[i], A[j]
+    A[l], A[j] = A[j], A[l]
+    return j
 
 def quicksort(A, l, r):
     if l >= r:
         return
-    k = random.randint(l, r+1)
-    A[0], A[k] = A[k], A[0]
+
+    k = np.random.randint(l, r+1)
+    A[l], A[k] = A[k], A[l]
 
     m = Partition(A, l, r)
 
     quicksort(A, l, m-1)
     quicksort(A, m+1, r)
 
-    return A
+# Test
+while True:
+    A = random.sample(range(1,100), 10)
+    initialA = A
+    quicksort(A, 0, len(A)-1)
+    initialA.sort(reverse=True)
+    if A == initialA:
+        print('OK')
+    else:
+        print('Error')
+        break
