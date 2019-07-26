5a58cb9069ad900656732b5dad014763b0688002
nsuresh <aqurilla@gmail.com>
2019-07-26 16:47:35 -0400

    Update Strings_1.py

--- algorithmic_toolbox/python/Strings_1.py
+++ algorithmic_toolbox/python/Strings_1.py
@@ -113,3 +113,22 @@ class Solution:
         else:
             return total
     
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        if not needle:
+            return 0
+        
+        i = 0
+        
+        while i <= (len(haystack) - len(needle)):
+            
+            if haystack[i] == needle[0]:
+                j = 0
+                while j < len(needle) and (haystack[i+j] == needle[j]):
+                    j += 1
+                if j == len(needle):
+                    return i
+            
+            i += 1
+                
+        return -1
