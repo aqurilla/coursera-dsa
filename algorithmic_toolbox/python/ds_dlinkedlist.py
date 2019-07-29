67a2d57094d6a86f642e8e5607d427dbc49427eb
nsuresh <aqurilla@gmail.com>
2019-07-29 15:31:13 -0400

    add shift(), unshift()

--- algorithmic_toolbox/python/ds_dlinkedlist.py
+++ algorithmic_toolbox/python/ds_dlinkedlist.py
@@ -38,6 +38,34 @@ class DoublyLinkedList:
         print(f'Pop value: {popNode.val}')
         popNode.prev = None
         return popNode
+    
+    def shift(self):
+        # Remove node from beginning of DLL
+        if self.length == 0:
+            print('Empty list!')
+            return
+        tmp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = tmp.next
+            self.head.prev = None
+        tmp.next = None
+        self.length -= 1
+        return tmp
+    
+    def unshift(self, val):
+        newNode = Node(val)
+        if self.length == 0:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.head.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+        self.length += 1
+        return self
             
     def traverse(self):
         # Func for traversing list
