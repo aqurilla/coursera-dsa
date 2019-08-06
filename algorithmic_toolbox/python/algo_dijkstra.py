034f29da54719fe627043b0ff8e907bbc068b6ba
nsuresh <aqurilla@gmail.com>
2019-08-05 21:35:24 -0400

    Update algo_dijkstra.py

--- algorithmic_toolbox/python/algo_dijkstra.py
+++ algorithmic_toolbox/python/algo_dijkstra.py
@@ -10,17 +10,36 @@ class WeightedGraph:
         self.adjacencyList[vertex1].append({'node':vertex2, 'weight':weight})
         self.adjacencyList[vertex2].append({'node':vertex1, 'weight':weight})
 
-
-
+class PriorityQueue:
+    def __init__(self):
+        self.values = []
+    
+    def enq(self, val, priority):
+        self.values.append((val, priority))
+        self.sort()
+    
+    def deq(self):
+        return self.values.pop(0)
+    
+    def sort(self):
+        self.values = sorted(self.values, key = lambda x: x[1])
 
 # Driver code
-wg = WeightedGraph()
-wg.addVertex("A")
-wg.addVertex("B")
-wg.addVertex("C")
-wg.addEdge("A", "B", 9)
-wg.addEdge("A", "C", 5)
-wg.addEdge("B", "C", 7)
-print(wg.adjacencyList)
+# wg = WeightedGraph()
+# wg.addVertex("A")
+# wg.addVertex("B")
+# wg.addVertex("C")
+# wg.addEdge("A", "B", 9)
+# wg.addEdge("A", "C", 5)
+# wg.addEdge("B", "C", 7)
+# print(wg.adjacencyList)
+
+pq = PriorityQueue()
+pq.enq("A", 1)
+pq.enq("B", 2)
+pq.enq("C", 3)
+
+print(pq.values)
+
 
         
