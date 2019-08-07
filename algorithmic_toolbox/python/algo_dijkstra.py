739bdea3119aeddf394d5efde7638485648b22fa
nsuresh <aqurilla@gmail.com>
2019-08-06 22:34:44 -0400

    Update algo_dijkstra.py

--- algorithmic_toolbox/python/algo_dijkstra.py
+++ algorithmic_toolbox/python/algo_dijkstra.py
@@ -1,3 +1,5 @@
+import math
+
 class WeightedGraph:
     def __init__(self):
         self.adjacencyList = {}
@@ -9,8 +11,46 @@ class WeightedGraph:
     def addEdge(self, vertex1, vertex2, weight):
         self.adjacencyList[vertex1].append({'node':vertex2, 'weight':weight})
         self.adjacencyList[vertex2].append({'node':vertex1, 'weight':weight})
-
+        
+    def dijkstra(self, start, finish):
+        nodes = PriorityQueue()
+        distances = {}
+        previous = {}
+        path = []
+        
+        # initialize
+        for vertex in self.adjacencyList:
+            if vertex == start:
+                distances[vertex] = 0
+                nodes.enq(vertex, 0)
+            else:
+                distances[vertex] = math.inf
+                nodes.enq(vertex, math.inf)
+            previous[vertex] = None
+        
+        while nodes.values:
+            minNode = nodes.deq()[0]
+            if minNode == finish:
+                # build return path
+                while previous[minNode]:
+                    path.append(minNode)
+                    minNode = previous[minNode]
+                break
+                
+            if (minNode or distances[minNode] != math.inf):
+                for nextNode in self.adjacencyList[minNode]:
+                    candidate = distances[minNode] + nextNode['weight']
+                    if candidate < distances[nextNode['node']]:
+                        # updating minimum distance
+                        distances[nextNode['node']] = candidate
+                        # updating previous node to neighbor for min distance
+                        previous[nextNode['node']] = minNode
+                        nodes.enq(nextNode['node'], candidate)
+        path.append(start)
+        return list(reversed(path))
+                         
 class PriorityQueue:
+    # simple pq
     def __init__(self):
         self.values = []
     
@@ -25,21 +65,23 @@ class PriorityQueue:
         self.values = sorted(self.values, key = lambda x: x[1])
 
 # Driver code
-# wg = WeightedGraph()
-# wg.addVertex("A")
-# wg.addVertex("B")
-# wg.addVertex("C")
-# wg.addEdge("A", "B", 9)
-# wg.addEdge("A", "C", 5)
-# wg.addEdge("B", "C", 7)
-# print(wg.adjacencyList)
+graph = WeightedGraph()
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
 
-pq = PriorityQueue()
-pq.enq("A", 1)
-pq.enq("B", 2)
-pq.enq("C", 3)
+graph.addEdge("A","B", 4);
+graph.addEdge("A","C", 2);
+graph.addEdge("B","E", 3);
+graph.addEdge("C","D", 2);
+graph.addEdge("C","F", 4);
+graph.addEdge("D","E", 3);
+graph.addEdge("D","F", 1);
+graph.addEdge("E","F", 1);
 
-print(pq.values)
-
+print(graph.dijkstra("A", "E"))
 
         
