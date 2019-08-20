05803779eff2e48e8a95caacf6c48521834c8e64
nsuresh <aqurilla@gmail.com>
2019-08-20 16:45:37 -0400

    Update Trees_lvl2.py

--- algorithmic_toolbox/python/Trees_lvl2.py
+++ algorithmic_toolbox/python/Trees_lvl2.py
@@ -23,3 +23,23 @@ class Solution(object):
         soln = []
         self.visitNode(soln, root)
         return soln
+    
+    def modTraversal(self, soln, root, counter, k):
+        
+        if root == None:
+            return
+        
+        self.modTraversal(soln, root.left, counter, k)
+        soln.append(root.val)
+        counter += 1
+        if counter >= k:
+            return
+        self.modTraversal(soln, root.right, counter, k)
+        
+    
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        soln = []
+        counter = 0
+        self.modTraversal(soln, root, counter, k)
+        
+        return soln[k-1]
