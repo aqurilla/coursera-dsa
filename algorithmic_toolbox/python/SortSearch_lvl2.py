d3d1c33a0c8d234baa53369354bf8f7687a816ef
nsuresh <aqurilla@gmail.com>
2019-08-24 22:10:13 -0400

    Update SortSearch_lvl2.py

--- algorithmic_toolbox/python/SortSearch_lvl2.py
+++ algorithmic_toolbox/python/SortSearch_lvl2.py
@@ -27,3 +27,29 @@ class Solution:
             return 0
         elif nums[len(nums)-1]>nums[len(nums)-2]:
             return len(nums)-1
+        
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        
+        left = 0
+        right = len(nums)-1
+        
+        while left <= right:
+            mid = left + (right - left)//2
+            if nums[mid] == target:
+                leftrange = mid
+                rightrange = mid
+                while leftrange > 0 and nums[leftrange-1] == target:
+                    leftrange -= 1
+                while rightrange < len(nums)-1 and nums[rightrange+1] == target:
+                    rightrange += 1
+                return [leftrange, rightrange]
+                    
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        # Not found
+        return [-1, -1]
