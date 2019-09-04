0ae13561b07457ba04ab5ffe83bf52329d05ad50
nsuresh <aqurilla@gmail.com>
2019-09-04 13:35:49 -0400

    Update DP_lvl2.py

--- algorithmic_toolbox/python/DP_lvl2.py
+++ algorithmic_toolbox/python/DP_lvl2.py
@@ -5,3 +5,21 @@
                 return False
             maxposindex = max(maxposindex, i+n)
         return True
+    
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0: return 0
+        numcoins = [math.inf for _ in range(amount+1)]
+        combpos = [-1 for _ in range(amount+1)]
+        numcoins[0] = 0
+        
+        for j in range(len(coins)):
+            for i in range(1, amount+1):
+                if i >= coins[j]:
+                    if (numcoins[i-coins[j]] + 1 < numcoins[i]):
+                        numcoins[i] = numcoins[i - coins[j]] + 1
+                        combpos[i] = j
+        
+        if combpos[amount] == -1:
+            return -1
+        else:
+            return numcoins[amount]
