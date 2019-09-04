4bf3a812c8a37ffb8d1aa8045c467d5d1881d455
nsuresh <aqurilla@gmail.com>
2019-09-04 12:17:23 -0400

    Update Backtracking_lvl2.py

--- algorithmic_toolbox/python/Backtracking_lvl2.py
+++ algorithmic_toolbox/python/Backtracking_lvl2.py
@@ -69,10 +69,3 @@ class Solution:
         if numleft < numright:
             self.genPar(genstr + ")", numleft, numright-1, res)
             
-    def canJump(self, nums: List[int]) -> bool:
-        maxposindex = 0
-        for i,n in enumerate(nums):
-            if i > maxposindex:
-                return False
-            maxposindex = max(maxposindex, i+n)
-        return True
