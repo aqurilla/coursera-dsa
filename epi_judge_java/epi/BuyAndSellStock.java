ebff6875b0e9f1dcc8bc680ceb6814ef7db9c42d
aqurilla <aqurilla@gmail.com>
2020-05-27 08:29:23 -0700

    added solution

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -2,11 +2,28 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.List;
+
 public class BuyAndSellStock {
+
+  /**
+   * Returns the maximum profit that could be made by buying
+   * and then selling one share of stock
+   * @param prices List of stock prices
+   * @return       Maximum possible profit
+   */
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    double maxProfit = Double.MIN_VALUE;
+    double currentMin = Double.MAX_VALUE;
+
+    // Iterate over the given list
+    for (Double price: prices) {
+      // Check if a better profit is possible
+      currentMin = Math.min(currentMin, price);
+      maxProfit = Math.max(maxProfit, price - currentMin);
+    }
+
+    return maxProfit;
   }
 
   public static void main(String[] args) {
