6ed339142f80aca81b6a33571649d33c0aac654a
aqurilla <aqurilla@gmail.com>
2020-06-10 20:45:36 -0700

    added solution

--- epi_judge_java/epi/SortedArrayRemoveDups.java
+++ epi_judge_java/epi/SortedArrayRemoveDups.java
@@ -1,27 +1,48 @@
 package epi;
+
+import java.util.List;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
-import java.util.List;
-public class SortedArrayRemoveDups {
-  // Returns the number of valid entries after deletion.
-  public static int deleteDuplicates(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
-  }
-  @EpiTest(testDataFile = "sorted_array_remove_dups.tsv")
-  public static List<Integer> deleteDuplicatesWrapper(TimedExecutor executor,
-                                                      List<Integer> A)
-      throws Exception {
-    int end = executor.run(() -> deleteDuplicates(A));
-    return A.subList(0, end);
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SortedArrayRemoveDups.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class SortedArrayRemoveDups {
+	// Returns the number of valid entries after deletion.
+	public static int deleteDuplicates(List<Integer> A) {
+
+		if (A.isEmpty()) {
+			return 0;
+		}
+
+		int i = 1;
+		int j = 0;
+
+		while (i < A.size()) {
+			while (i < A.size() && A.get(i).equals(A.get(j))) {
+				i++;
+			}
+
+			// if end of list
+			if (i >= A.size()) {
+				break;
+			}
+
+			// else set element at j
+			j++;
+			A.set(j, A.get(i));
+		}
+
+		return j + 1;
+	}
+
+	@EpiTest(testDataFile = "sorted_array_remove_dups.tsv")
+	public static List<Integer> deleteDuplicatesWrapper(TimedExecutor executor, List<Integer> A) throws Exception {
+		int end = executor.run(() -> deleteDuplicates(A));
+		return A.subList(0, end);
+	}
+
+	public static void main(String[] args) {
+		System.exit(GenericTest.runFromAnnotations(args, "SortedArrayRemoveDups.java", new Object() {
+		}.getClass().getEnclosingClass()).ordinal());
+	}
 }
