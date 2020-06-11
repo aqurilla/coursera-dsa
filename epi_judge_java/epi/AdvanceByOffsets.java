3de3c7234311558e555900faf31c92f83565595c
aqurilla <aqurilla@gmail.com>
2020-06-11 10:12:59 -0700

    added solution

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -1,19 +1,43 @@
 package epi;
+
+import java.util.List;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
-public class AdvanceByOffsets {
-  @EpiTest(testDataFile = "advance_by_offsets.tsv")
-  public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
-    // TODO - you fill in here.
-    return true;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "AdvanceByOffsets.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class AdvanceByOffsets {
+	@EpiTest(testDataFile = "advance_by_offsets.tsv")
+	public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
+
+		if (maxAdvanceSteps.isEmpty()) {
+			return false;
+		}
+
+		int maxReachable = maxAdvanceSteps.get(0);
+
+		for (int i = 0; i < maxAdvanceSteps.size(); i++) {
+
+			// If current index greater than maximum reachable
+			// cannot reach end
+			if (i > maxReachable) {
+				return false;
+			}
+
+			// Update maxReachable if required
+			maxReachable = Math.max(maxReachable, i + maxAdvanceSteps.get(i));
+
+			// if possible to reach end
+			if (maxReachable >= maxAdvanceSteps.size() - 1) {
+				return true;
+			}
+
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.exit(GenericTest.runFromAnnotations(args, "AdvanceByOffsets.java", new Object() {
+		}.getClass().getEnclosingClass()).ordinal());
+	}
 }
