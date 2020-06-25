74a41e7f3fc2e26e51ef0935e2c73198123171ba
aqurilla <aqurilla@gmail.com>
2020-06-24 17:10:09 -0700

    added queuestack solution

--- epi_judge_java/epi/QueueFromStacks.java
+++ epi_judge_java/epi/QueueFromStacks.java
@@ -1,23 +1,49 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
+
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
+
 public class QueueFromStacks {
 
   public static class Queue {
+
+    // Only stacks can be used
+    private Deque<Integer> stack1 = new LinkedList<>();
+    private Deque<Integer> stack2 = new LinkedList<>();
+
     public void enqueue(Integer x) {
-      // TODO - you fill in here.
+      stack1.addFirst(x);
       return;
     }
+
     public Integer dequeue() {
-      // TODO - you fill in here.
-      return 0;
+
+      // once s1 elems are transfered to s2
+      // dequeues can be done till s2 is empty
+      // enqueues are automatically valid into s1
+
+      if (stack2.isEmpty()) {
+        // reverse order into stack2
+        while (!stack1.isEmpty()) {
+          stack2.addFirst(stack1.removeFirst());
+        }
+      }
+
+      // pop last element from s2
+      int elem = stack2.removeFirst();
+
+      return elem;
     }
   }
-  @EpiUserType(ctorParams = {String.class, int.class})
+
+  @EpiUserType(ctorParams = { String.class, int.class })
   public static class QueueOp {
     public String op;
     public int arg;
@@ -35,20 +61,18 @@ public class QueueFromStacks {
 
       for (QueueOp op : ops) {
         switch (op.op) {
-        case "QueueWithMax":
-          q = new Queue();
-          break;
-        case "enqueue":
-          q.enqueue(op.arg);
-          break;
-        case "dequeue":
-          int result = q.dequeue();
-          if (result != op.arg) {
-            throw new TestFailure("Dequeue: expected " +
-                                  String.valueOf(op.arg) + ", got " +
-                                  String.valueOf(result));
-          }
-          break;
+          case "QueueWithMax":
+            q = new Queue();
+            break;
+          case "enqueue":
+            q.enqueue(op.arg);
+            break;
+          case "dequeue":
+            int result = q.dequeue();
+            if (result != op.arg) {
+              throw new TestFailure("Dequeue: expected " + String.valueOf(op.arg) + ", got " + String.valueOf(result));
+            }
+            break;
         }
       }
     } catch (NoSuchElementException e) {
@@ -57,10 +81,7 @@ public class QueueFromStacks {
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "QueueFromStacks.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "QueueFromStacks.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
