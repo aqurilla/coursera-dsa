e36c2e23d550da14eb5594efeeb0083a2a5ea882
aqurilla <aqurilla@gmail.com>
2020-06-26 20:50:26 -0700

    added symmetric tree solution

--- epi_judge_java/epi/IsTreeSymmetric.java
+++ epi_judge_java/epi/IsTreeSymmetric.java
@@ -1,19 +1,46 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class IsTreeSymmetric {
-  @EpiTest(testDataFile = "is_tree_symmetric.tsv")
 
+public class IsTreeSymmetric {
+
+  @EpiTest(testDataFile = "is_tree_symmetric.tsv")
+  /**
+   * Checks if a given tree is symmetric
+   * 
+   * @param tree
+   * @return
+   */
   public static boolean isSymmetric(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    return tree == null || twoSymmetric(tree.left, tree.right);
+  }
+
+  /**
+   * Check whether data is same, and whether it is mirror
+   * 
+   * @param node1
+   * @param node2
+   * @return
+   */
+  public static boolean twoSymmetric(BinaryTreeNode<Integer> node1, BinaryTreeNode<Integer> node2) {
+
+    if (node1 == null && node2 == null) {
+      return true;
+    }
+
+    if (node1 != null && node2 != null) {
+      return (node1.data == node2.data) && twoSymmetric(node1.left, node2.right)
+          && twoSymmetric(node1.right, node2.left);
+    }
+
+    // only one node is null
+    return false;
+
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeSymmetric.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "IsTreeSymmetric.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
--- epi_judge_java/epi/LowestCommonAncestor.java
+++ epi_judge_java/epi/LowestCommonAncestor.java
@@ -1,25 +1,32 @@
 package epi;
+
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
 public class LowestCommonAncestor {
-  public static BinaryTreeNode<Integer> LCA(BinaryTreeNode<Integer> tree,
-                                            BinaryTreeNode<Integer> node0,
-                                            BinaryTreeNode<Integer> node1) {
-    // TODO - you fill in here.
+  public static BinaryTreeNode<Integer> LCA(BinaryTreeNode<Integer> tree, BinaryTreeNode<Integer> node0,
+      BinaryTreeNode<Integer> node1) {
+
+    // for a node, check if left and right contain given nodes
+
     return null;
   }
+
+  public static boolean hasNode(BinaryTreeNode<Integer> tree, BinaryTreeNode<Integer> node0,
+      BinaryTreeNode<Integer> node1) {
+
+  }
+
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
-  public static int lcaWrapper(TimedExecutor executor,
-                               BinaryTreeNode<Integer> tree, Integer key0,
-                               Integer key1) throws Exception {
+  public static int lcaWrapper(TimedExecutor executor, BinaryTreeNode<Integer> tree, Integer key0, Integer key1)
+      throws Exception {
     BinaryTreeNode<Integer> node0 = BinaryTreeUtils.mustFindNode(tree, key0);
     BinaryTreeNode<Integer> node1 = BinaryTreeUtils.mustFindNode(tree, key1);
 
-    BinaryTreeNode<Integer> result =
-        executor.run(() -> LCA(tree, node0, node1));
+    BinaryTreeNode<Integer> result = executor.run(() -> LCA(tree, node0, node1));
 
     if (result == null) {
       throw new TestFailure("Result can not be null");
@@ -28,10 +35,7 @@ public class LowestCommonAncestor {
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "LowestCommonAncestor.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "LowestCommonAncestor.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
