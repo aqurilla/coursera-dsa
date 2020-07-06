29007c92311a6a2ef4cfe85ca48b4d41eeee8ba1
aqurilla <aqurilla@gmail.com>
2020-07-06 09:18:05 -0700

    added kStars solution

--- epi_judge_java/epi/KClosestStars.java
+++ epi_judge_java/epi/KClosestStars.java
@@ -1,15 +1,20 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.EpiTestExpectedType;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.function.BiPredicate;
+
 public class KClosestStars {
-  @EpiUserType(ctorParams = {double.class, double.class, double.class})
+  @EpiUserType(ctorParams = { double.class, double.class, double.class })
 
   public static class Star implements Comparable<Star> {
     private double x, y, z;
@@ -20,7 +25,9 @@ public class KClosestStars {
       this.z = z;
     }
 
-    public double distance() { return Math.sqrt(x * x + y * y + z * z); }
+    public double distance() {
+      return Math.sqrt(x * x + y * y + z * z);
+    }
 
     @Override
     public int compareTo(Star that) {
@@ -34,19 +41,40 @@ public class KClosestStars {
   }
 
   public static List<Star> findClosestKStars(Iterator<Star> stars, int k) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
+
+    // use a size k max-heap to store k closest stars
+    // Collections.reverseOrder used to get reverse of natural ordering
+    // specified by the Star's compareTo method
+    PriorityQueue<Star> maxHeap = new PriorityQueue<>(k, Collections.reverseOrder());
+
+    while (stars.hasNext()) {
+      if (maxHeap.size() < k) {
+        maxHeap.add(stars.next());
+      } else {
+        // Check if next star is smaller than the
+        // furthest one among the current k stars
+        Star nextStar = stars.next();
+        Star maxClosest = maxHeap.peek();
+        if (nextStar.compareTo(maxClosest) < 0) {
+          maxHeap.poll();
+          maxHeap.add(nextStar);
+        }
+      }
+    }
+
+    return new ArrayList<>(maxHeap);
   }
+
   @EpiTest(testDataFile = "k_closest_stars.tsv")
   public static List<Star> findClosestKStarsWrapper(List<Star> stars, int k) {
     return findClosestKStars(stars.iterator(), k);
   }
 
-  @EpiTestExpectedType public static List<Double> expectedType;
+  @EpiTestExpectedType
+  public static List<Double> expectedType;
 
   @EpiTestComparator
-  public static BiPredicate<List<Double>, List<Star>> comp =
-      (expected, result) -> {
+  public static BiPredicate<List<Double>, List<Star>> comp = (expected, result) -> {
     if (expected.size() != result.size()) {
       return false;
     }
@@ -60,10 +88,7 @@ public class KClosestStars {
   };
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "KClosestStars.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "KClosestStars.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
