b16d665da63872fccfceba9490ddcc9295a86d41
aqurilla <aqurilla@gmail.com>
2020-07-08 11:59:28 -0700

    added canBePalindrome solution

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,19 +1,46 @@
 package epi;
+
+import java.util.HashMap;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
-    // TODO - you fill in here.
-    return true;
+
+    // What strings can form palindromes?
+    char[] charArray = s.toCharArray();
+    HashMap<Character, Integer> charMap = new HashMap<>();
+    int numberOfOddChars = 0;
+
+    // Find numbers of each character
+    for (Character c : charArray) {
+      if (!charMap.containsKey(c)) {
+        charMap.put(c, 1);
+      } else {
+        charMap.put(c, charMap.get(c) + 1);
+      }
+    }
+
+    for (int value : charMap.values()) {
+      if (value % 2 != 0) {
+        numberOfOddChars++;
+      }
+      // if more than 1 char with odd total
+      // impossible to form palindrome
+      if (numberOfOddChars > 1) {
+        return false;
+      }
+    }
+
+    return numberOfOddChars == 0 ? true : s.length() % 2 != 0;
+
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsStringPermutableToPalindrome.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "IsStringPermutableToPalindrome.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
