2fdf6a3575f45f0c272d1377849c59ed647a8ed9
aqurilla <aqurilla@gmail.com>
2020-07-08 12:36:15 -0700

    added anonymousTextPossible solution

--- epi_judge_java/epi/IsAnonymousLetterConstructible.java
+++ epi_judge_java/epi/IsAnonymousLetterConstructible.java
@@ -1,20 +1,59 @@
 package epi;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsAnonymousLetterConstructible {
   @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
-  public static boolean isLetterConstructibleFromMagazine(String letterText,
-                                                          String magazineText) {
-    // TODO - you fill in here.
-    return true;
+  private static HashMap<Character, Integer> createMap(String text) {
+
+    HashMap<Character, Integer> charMap = new HashMap<>();
+
+    for (Character c : text.toCharArray()) {
+
+      if (!charMap.containsKey(c)) {
+        charMap.put(c, 0);
+      } else {
+        charMap.put(c, charMap.get(c) + 1);
+      }
+
+    }
+
+    return charMap;
+
+  }
+
+  public static boolean isLetterConstructibleFromMagazine(String letterText, String magazineText) {
+
+    // create hashmap of letter
+    HashMap<Character, Integer> letterMap = createMap(letterText);
+
+    // go through magazine, till the letter's hashmap becomes empty
+    for (Character c : magazineText.toCharArray()) {
+
+      if (letterMap.containsKey(c)) {
+        letterMap.put(c, letterMap.get(c) - 1);
+
+        if (letterMap.get(c) == 0) {
+          letterMap.remove(c);
+          // if hashmap becomes empty, break
+          if (letterMap.isEmpty()) {
+            break;
+          }
+        }
+      }
+    }
+
+    // if letter's hashmap still non-empty, return false
+    return letterMap.isEmpty();
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsAnonymousLetterConstructible.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "IsAnonymousLetterConstructible.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
