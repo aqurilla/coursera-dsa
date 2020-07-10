e228dcfba9990b134c7af212c8c609464f6f59d4
aqurilla <aqurilla@gmail.com>
2020-07-09 21:18:12 -0700

    added lca solution

--- epi_judge_java/epi/LowestCommonAncestorCloseAncestor.java
+++ epi_judge_java/epi/LowestCommonAncestorCloseAncestor.java
@@ -1,19 +1,56 @@
 package epi;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
 public class LowestCommonAncestorCloseAncestor {
 
-  public static BinaryTree<Integer> LCA(BinaryTree<Integer> node0,
-                                        BinaryTree<Integer> node1) {
-    // TODO - you fill in here.
+  public static BinaryTree<Integer> LCA(BinaryTree<Integer> node0, BinaryTree<Integer> node1) {
+
+    // if same node
+    if (node0 == node1) {
+      return node0;
+    }
+
+    // store visited nodes in a hashset
+    Set<BinaryTree<Integer>> visitedNodes = new HashSet<>();
+
+    // the first common node is the LCA
+    while (node0 != null || node1 != null) {
+
+      // if node is not null
+      if (node0 != null) {
+        if (visitedNodes.contains(node0)) {
+          return node0;
+        } else {
+          visitedNodes.add(node0);
+        }
+        node0 = node0.parent;
+      }
+
+      if (node1 != null) {
+        if (visitedNodes.contains(node1)) {
+          return node1;
+        } else {
+          visitedNodes.add(node1);
+        }
+        node1 = node1.parent;
+      }
+
+    }
+
     return null;
   }
+
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
-  public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree,
-                               Integer key0, Integer key1) throws Exception {
+  public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree, Integer key0, Integer key1)
+      throws Exception {
     BinaryTree<Integer> node0 = BinaryTreeUtils.mustFindNode(tree, key0);
     BinaryTree<Integer> node1 = BinaryTreeUtils.mustFindNode(tree, key1);
 
@@ -26,10 +63,7 @@ public class LowestCommonAncestorCloseAncestor {
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "LowestCommonAncestorCloseAncestor.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "LowestCommonAncestorCloseAncestor.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
--- epi_judge_java/epi/LruCache.java
+++ epi_judge_java/epi/LruCache.java
@@ -1,25 +1,59 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
+
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LruCache {
-  LruCache(final int capacity) {}
-  public Integer lookup(Integer key) {
-    // TODO - you fill in here.
-    return 0;
+
+  LinkedHashMap<Integer, Integer> isbnMap;
+
+  LruCache(final int capacity) {
+
+    this.isbnMap = new LinkedHashMap<Integer, Integer>(capacity, 1.0f, true) {
+      /**
+       *
+       */
+      private static final long serialVersionUID = 1L;
+
+      @Override
+      protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return this.size() > capacity;
+      }
+    };
+
   }
+
+  public Integer lookup(Integer key) {
+
+    if (!isbnMap.containsKey(key)) {
+      return null;
+    }
+
+    return isbnMap.get(key);
+  }
+
   public void insert(Integer key, Integer value) {
-    // TODO - you fill in here.
+
+    isbnMap.get(key);
+    // if already exists, dont change price
+    if (!isbnMap.containsKey(key)) {
+      isbnMap.put(key, value);
+    }
+
     return;
   }
+
   public Boolean erase(Object key) {
-    // TODO - you fill in here.
-    return true;
+    return isbnMap.remove(key) != null;
   }
-  @EpiUserType(ctorParams = {String.class, int.class, int.class})
+
+  @EpiUserType(ctorParams = { String.class, int.class, int.class })
   public static class Op {
     String code;
     int arg1;
@@ -41,34 +75,29 @@ public class LruCache {
     for (Op op : commands.subList(1, commands.size())) {
       int result;
       switch (op.code) {
-      case "lookup":
-        result = cache.lookup(op.arg1);
-        if (result != op.arg2) {
-          throw new TestFailure("Lookup: expected " + String.valueOf(op.arg2) +
-                                ", got " + String.valueOf(result));
-        }
-        break;
-      case "insert":
-        cache.insert(op.arg1, op.arg2);
-        break;
-      case "erase":
-        result = cache.erase(op.arg1) ? 1 : 0;
-        if (result != op.arg2) {
-          throw new TestFailure("Erase: expected " + String.valueOf(op.arg2) +
-                                ", got " + String.valueOf(result));
-        }
-        break;
-      default:
-        throw new RuntimeException("Unexpected command " + op.code);
+        case "lookup":
+          result = cache.lookup(op.arg1);
+          if (result != op.arg2) {
+            throw new TestFailure("Lookup: expected " + String.valueOf(op.arg2) + ", got " + String.valueOf(result));
+          }
+          break;
+        case "insert":
+          cache.insert(op.arg1, op.arg2);
+          break;
+        case "erase":
+          result = cache.erase(op.arg1) ? 1 : 0;
+          if (result != op.arg2) {
+            throw new TestFailure("Erase: expected " + String.valueOf(op.arg2) + ", got " + String.valueOf(result));
+          }
+          break;
+        default:
+          throw new RuntimeException("Unexpected command " + op.code);
       }
     }
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "LruCache.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "LruCache.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
