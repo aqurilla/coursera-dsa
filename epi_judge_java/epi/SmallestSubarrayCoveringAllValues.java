df7d58c1202bda6dfe29abe1760b21ca7fe9dd44
aqurilla <aqurilla@gmail.com>
2020-07-10 17:35:33 -0700

    added coverSubArray soln

--- epi_judge_java/epi/SmallestSubarrayCoveringAllValues.java
+++ epi_judge_java/epi/SmallestSubarrayCoveringAllValues.java
@@ -1,9 +1,14 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class SmallestSubarrayCoveringAllValues {
 
   public static class Subarray {
@@ -17,18 +22,58 @@ public class SmallestSubarrayCoveringAllValues {
     }
   }
 
-  public static Subarray
-  findSmallestSequentiallyCoveringSubset(List<String> paragraph,
-                                         List<String> keywords) {
-    // TODO - you fill in here.
-    return new Subarray(0, 0);
+  public static Subarray findSmallestSequentiallyCoveringSubset(List<String> paragraph, List<String> keywords) {
+
+    // use a 2-pointer approach to identify the minimum subarray
+    Map<String, Integer> stringMap = new HashMap<>();
+
+    // fill the map
+    for (String keyword : keywords) {
+      stringMap.put(keyword, stringMap.containsKey(keyword) ? stringMap.get(keyword) + 1 : 1);
+    }
+
+    Subarray soln = new Subarray(-1, -1);
+    int balance = keywords.size();
+
+    for (int left = 0, right = 0; right < paragraph.size(); right++) {
+
+      Integer keywordCount = stringMap.get(paragraph.get(right));
+      if (keywordCount != null) {
+        keywordCount--;
+        stringMap.put(paragraph.get(right), keywordCount);
+        if (keywordCount >= 0) {
+          balance--;
+        }
+      }
+
+      // if it covers all keywords, move left forwards
+      while (balance == 0) {
+
+        // smaller range found
+        if ((soln.start == -1 && soln.end == -1) || right - left < soln.end - soln.start) {
+          soln.start = left;
+          soln.end = right;
+        }
+
+        // if removed element is a keyword, add back to hashmap
+        keywordCount = stringMap.get(paragraph.get(left));
+        if (keywordCount != null) {
+          keywordCount++;
+          stringMap.put(paragraph.get(left), keywordCount);
+          if (keywordCount > 0) {
+            balance++;
+          }
+        }
+        left++;
+      }
+    }
+    return soln;
   }
+
   @EpiTest(testDataFile = "smallest_subarray_covering_all_values.tsv")
-  public static int findSmallestSequentiallyCoveringSubsetWrapper(
-      TimedExecutor executor, List<String> paragraph, List<String> keywords)
-      throws Exception {
-    Subarray result = executor.run(
-        () -> findSmallestSequentiallyCoveringSubset(paragraph, keywords));
+  public static int findSmallestSequentiallyCoveringSubsetWrapper(TimedExecutor executor, List<String> paragraph,
+      List<String> keywords) throws Exception {
+    Subarray result = executor.run(() -> findSmallestSequentiallyCoveringSubset(paragraph, keywords));
 
     int kwIdx = 0;
     if (result.start < 0) {
@@ -52,10 +97,7 @@ public class SmallestSubarrayCoveringAllValues {
   }
 
   public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SmallestSubarrayCoveringAllValues.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+    System.exit(GenericTest.runFromAnnotations(args, "SmallestSubarrayCoveringAllValues.java", new Object() {
+    }.getClass().getEnclosingClass()).ordinal());
   }
 }
